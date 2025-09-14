62cbe55dc618c0999075279ac81157f29729b904
yaansz <yancfm@gmail.com>
2025-09-14 15:07:00 -0300

    mobile will open the youtube link

--- src/app/components/project-info-component/project-info-component.ts
+++ src/app/components/project-info-component/project-info-component.ts
@@ -7,6 +7,8 @@ import { TagModule } from 'primeng/tag';
 import { DialogModule } from 'primeng/dialog';
 import { YoutubePlayerComponent } from "../youtube-player-component/youtube-player-component";
 import { TranslatePipe } from '@ngx-translate/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { Platform } from '@angular/cdk/platform';
 
 
 @Component({
@@ -19,10 +21,22 @@ export class ProjectInfoComponent {
 
   youtubeDialog : boolean = false;
 
+  constructor(private deviceService : DeviceDetectorService) {}
+
   @Input() project : ProjectsContent | null = null;
 
-  openDialog() {
-    this.youtubeDialog = true;
+  openVideo() {
+    console.log(this.deviceService.getDeviceInfo);
 
+    if(this.isMobile() && this.project?.youtube) {
+      window.open(`https://www.youtube.com/watch?v=${this.project?.youtube}`, '_blank');
+    } else if(this.project?.youtube) {
+      this.youtubeDialog = true;
+    }
+  }
+
+  private isMobile(): boolean {
+    return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    // Or use Angular CDK Platform: return this.platform.ANDROID || this.platform.IOS;
   }
 }
